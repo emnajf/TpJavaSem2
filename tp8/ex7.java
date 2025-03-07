d27c00faabf5d2a45ac505d5f2ffe2686049a8ce
unknown <eemna177@gmail.com>
2025-03-07 11:48:45 +0100

    dossir valide

--- tp8/ex7.java
+++ tp8/ex7.java
@@ -1,25 +1,24 @@
 import java.util.Scanner;
-public class ex7{
-    public static void main(String[]args){
-        Scanner scanner=new Scanner(System.in);
-        String verbe,radicale;
-     do{
-        System.out.println("Entrez un verbe:");
-        verbe=scanner.nextLine();
-    } While (!verbe.endsWith("er") || verbe.equals("aller"));
-
-
-System.out.println("je "+verbe.replace("er","e"));
-System.out.println("tu "+verbe.replace("er","es"));
-System.out.println("il "+verbe.replace("er","e"));
-System.out.println("elle "+verbe.replace("er","e"));
-if (verbe.equals("manger")) {
-    System.out.println("nous "+verbe.replace("er","ons"));
-} else if (verbe.equals("commencer")){
-   System.out.println("nous "+verbe.replace("er","ons")); 
-}
-    System.out.println("vous "+verbe.replace("er","ez"));
-    System.out.println("ils "+verbe.replace("er","ent"));
-    System.out.println("elles "+verbe.replace("er","ent"));
-            
+public class ex7 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String   verb ;
+    do {
+        System.out.println("enterz un verbe");
+        verb = sc.nextLine();
+    } while (!verb.endsWith("er") || verb.equals("aller")); 
+    System.out.println("entrez un entre verbe");
+    System.out.println("je "+verb.replace("er", "e"));
+    System.out.println("tu "+verb.replace("er", "es"));
+    System.out.println("il "+verb.replace("er", "e"));
+    System.out.println("elle "+verb.replace("er", "e"));
+    if (verb.equals("manger")) {
+        System.out.println("nous "+ verb.replace("er", "eons"));
+    }else if (verb.equals("commencer")) {
+        System.out.println("nous "+ verb.replace("cer", "çons"));
+    }
+    System.out.println("vous "+verb.replace("er", "ez"));
+    System.out.println("ils "+verb.replace("er", "ent"));
+    System.out.println("elles "+verb.replace("er", "ent"));
+     }
 }
